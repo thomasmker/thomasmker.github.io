23719783d50c20e1710e2c35dacf4c30463b74a1
Thomas Hayashi <thomashsh11@gmail.com>
2021-12-18 15:19:44 -0300

    Tooltip and dark mode adjustment

--- javascript/my_script.js
+++ javascript/my_script.js
@@ -7,5 +7,11 @@ function changeTheme() {
    var cards = document.querySelectorAll(".card");
    for (let i = 0; i < cards.length; i++) {
         cards[i].classList.toggle("dark-mode");
-    }
+   }
+
+   // Dark mode to the carousel icons
+   var carousel_icons = document.querySelectorAll(".carousel-control-prev-icon, .carousel-control-next-icon")
+   for (let i = 0; i < carousel_icons.length; i++) {
+        carousel_icons[i].classList.toggle("carousel-dark-mode-button");
+   }
 }
